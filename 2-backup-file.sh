1ba865b2d713bc730f77ef5fae584c4f5b46602a
paojn <paojn@mail.uc.edu>
2023-10-17 23:57:18 -0400

    ran backup-file.sh successfully

--- 2-backup-file.sh
+++ 2-backup-file.sh
@@ -6,23 +6,25 @@ read -p "Enter a file name or path: " filename
 # Check if the specified file exists
 if [ -f "$filename" ]; then
   # Get the current date and time
-  current_date= # in the format YYYY-MM-DD
-  current_time= # in the format HH-MM-SS
+  current_date=$(date +'%Y-%m-%d') # in the format YYYY-MM-DD
+  current_time=$(date +'%H-%M-%S') # in the format HH-MM-SS
 
   # Create the backup file name by appending the current date and time
   # Be sure to add the date and time between the file name and extension (ex. README_2020-01-01_12-00-00.md)
-  backup_filename="${filename%.*}.${filename##*.}"
+  extension="${filename##*.}"
+  filename_noext="${filename%.*}"
+  backup_filename="${filename_noext}_${current_date}_${current_time}.$extension"
 
   # Copy the original file to the backup file
-
+  cp "$filename" "$backup_filename"
 
   # Compress the backup file using tar
-
+  tar -czvf "${backup_filename}.tar.gz" "$backup_filename"
 
   # Remove the uncompressed backup file
+  rm "$backup_filename"
 
-
-  echo "Backup file '$backup_filename.tar.gz' created and compressed successfully."
+  echo "Backup file '${backup_filename}.tar.gz' created and compressed successfully."
 else
   echo "Error: File '$filename' does not exist."
 fi
